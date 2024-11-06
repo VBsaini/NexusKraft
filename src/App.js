4245c4c152e458f0c1a7ca334f974f1792e4991c
VBsaini <vbsaini2004@gmail.com>
2024-11-07 01:28:40 +0530

    add general-sans font and latestProject section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./fonts/css/general-sans.css";
 import Navbar from "./components/common/Navbar";
 import HomeImage from "./components/pages/HomeImages/HomeImages";
 import HeroSection from "./components/pages/HeroSection/HeroSection";
+import LatestProjects from "./components/pages/projects/LatestProjects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HeroSection />
-      <HomeImage />
+      <div className="websiteContainer">
+        <HeroSection />
+        <HomeImage />
+        <LatestProjects />
+      </div>
     </div>
   );
 }
---
+++ src/components/hooks/ProjectCard/ProjectCard.js
@@ -0,0 +1,25 @@
+import { Stack } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { CgArrowTopRight } from "react-icons/cg";
+import { TfiArrowTopRight } from "react-icons/tfi";
+import "./ProjectCard.css";
+
+function ProjectCard(props) {
+  let { name, link, image } = props;
+  return (
+    <Card style={{ width: "20rem", border: "0" }}>
+      <Card.Img style={{ borderRadius: "20px" }} variant="top" src={image} />
+      <Card.Body className="p-0 m-2">
+        <Stack direction="horizontal">
+          <p className="p-0 m-0 project_name">{name}</p>
+          <a href={link} className="ms-auto align-top p-0 m-0 link">
+            View Project <TfiArrowTopRight />
+          </a>
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProjectCard;
