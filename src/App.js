d9ecca5e97993fd5eadd8b13a52d62a96902ab84
VBsaini <vbsaini2004@gmail.com>
2024-11-27 22:51:01 +0530

    main page res

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import "./Responsive.css";
 import "./fonts/css/general-sans.css";
 import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import Home from "./components/pages/Home/Home";
 import AboutUs from "./components/pages/AboutUs/AboutUs";
+import "./Responsive.css";
 
 function App() {
   return (
